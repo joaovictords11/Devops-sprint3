a4eb1822ac6cddc503a52f41467f7e9f2813f05e
Joao Victor de Souza <joaovictordesouza9285@gmail.com>
2025-11-08 21:40:20 -0300

    Limpa arquivos ignoráveis do repositório conforme .gitignore

--- src/main/java/com/exemplo/patio/PatioApplication.java
+++ src/main/java/com/exemplo/patio/PatioApplication.java
@@ -1,15 +1,7 @@
 package com.exemplo.patio;
 
-import com.exemplo.patio.model.Moto;
-import com.exemplo.patio.model.Registro;
-import com.exemplo.patio.repository.MotoRepository;
-import com.exemplo.patio.repository.RegistroRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class PatioApplication {
